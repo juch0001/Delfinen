4e18f2a7803b582317b79e93fa45fdb25ba0d6fe
AndreaRyholt <anra0003@stud.kea.dk>
2023-11-24 12:54:06 +0100

    Changes by julie

--- src/main/java/domainmodel/Member.java
+++ src/main/java/domainmodel/Member.java
@@ -17,4 +17,65 @@ public class Member {
         this.status = status;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public double getDebt() {
+        return debt;
+    }
+
+    public boolean getStatus(){
+        return status;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName (String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge (int age){
+        this.age = age;
+    }
+
+    public void setDebt (double debt){
+        this.debt = debt;
+    }
+
+    public void setStatus (boolean status){
+       this.status = status;
+    }
+
+    public void setTeam (Team team){
+        this.team = team;
+    }
+    public String statusToString(boolean status){
+        if (status){
+            return "Aktiv";
+        }else return "Passiv";
+    }
+
 }
